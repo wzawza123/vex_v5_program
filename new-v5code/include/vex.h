6a1a45dd1c9b88d3231b8301238215e04e2bab08
wza <936956317@qq.com>
2021-10-25 19:54:58 +0800

    Program for new car

---
+++ new-v5code/include/vex.h
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "v5.h"
+#include "v5_vcs.h"
+vex::brain Brain;
+vex::controller Controller = vex::controller();
+#define DIS(a) a.distance(vex::distanceUnits::mm)
+#define MOTOR(a,b,c) vex::motor a =vex::motor(vex::PORT##b,c);
+MOTOR(rf,6,false)MOTOR(rm1,8,true)MOTOR(rm2,9,true)MOTOR(rb,10,false)
+MOTOR(lf,1,true)MOTOR(lm1,2,false)MOTOR(lm2,3,false)MOTOR(lb,4,true)
+MOTOR(lup,11,true)MOTOR(rup,20,false)
+MOTOR(inhale,12,true)
+MOTOR(frontarm1,17,false)
+MOTOR(frontarm2,18,true)
+MOTOR(frontpaw,19,false)
+//MOTOR(dup,1,true)MOTOR(uup,3,false)
+#define VSEC vex::timeUnits::sec
+#define VDEG vex::rotationUnits::deg
+#define VPCT vex::velocityUnits::pct
+#define VRPM vex::velocityUnits::rpm
+#define VFWD vex::directionType::fwd
+#define VST(a,b); vexMotorVoltageSet(vex::PORT##a, b);//a is port b is
+#define ROTS(a,b,c); a.startRotateTo(b,VDEG,c,VPCT);//a is motor ,b is deg,c is pct
+#define ROT(a,b,c); a.rotateTo(b,VDEG,c,VPCT);//a is motor ,b is deg,c is pct
+#define ROFS(a,b,c); a.startRotateFor(b,VDEG,c,VPCT);//a is motor ,b is deg,c is pct
+#define ROF(a,b,c); a.rotateFor(b,VDEG,c,VPCT);//
+#define RST(a); a.resetRotation();//a is motor
+#define DEG(a) a.rotation(VDEG)//a is motor
+#define SP(a,b); {a.spin(VFWD,b,VPCT);}//a is motor
+#define AV(a) Controller.Axis##a.value()//a is controller Axis
+#define BP(a) Controller.Button##a.pressing()//a is controller Button
+#define SLEEP(a); vex::task::sleep(a);//a is time
+#define LIM(a) (a>100?100:(a<-100?-100:a))//no use
+#define CPC (M_PI*10.16)//M_PI is 3.14....
+#define AD(cm) (360*cm/CPC)//turn degree into cm
+#define VVST(a,b,c){VST(a,(c)*120)VST(b,(c)*120)}
+#define VSTT(a,c){VST(a,c)}
+#define FAV(a)(abs(AV(a))<15?0:(100.0*AV(a)/127))
---
+++ new-v5code/test.md
@@ -0,0 +1,39 @@
+# test note
+
+## ring installing
+
+with long nut columns
+
+### exp1
+
+#### background
+
+* at speed 40
+
+* manually set the ring and the arm angle
+* no car movement
+
+#### result
+
+7/10 with 2 dropped and 1 passing by
+
+<u>it seems the result highly depends on the attitude when taking in</u>
+
+- [ ] we cannot take in the transverse ring
+- [ ] the ribbon at the bottom could stuck in the ground
+- [ ] rings could be stuck with inappropriate distance
+- [ ] rings could pass by the column
+- [ ] rings could drop sideways
+
+### exp2
+
+#### background
+
+* at speed 45
+
+#### result
+
+5/10 with 5 passing by
+
+<p style="color:red;">trying to use short nut columns</p>
+
